380758782c4ac361bdd9cb792b5ff15e999b63fb
Murage <davidmurage290@gmail.com>
2025-07-16 18:51:27 +0300

    test if postgres is connected

--- app/database.py
+++ app/database.py
@@ -9,18 +9,27 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-#
-# while True:
-#     try:
-#         conn = psycopg.connect(host= 'localhost', user= 'murage90', dbname= 'my_dat',
-#                            password= 'reallynigga23',row_factory=dict_row  )
-#         cursor = conn.cursor()
-#         print("Connected to database successfully")
-#         break
-#     except Exception as error:
-#         print("Failed to connect to database")
-#         print("Error:", error)
-#         time.sleep(2)
+
+while True:
+     try:
+         conn = psycopg.connect(host= settings.database_hostname, user= settings.database_username, dbname= settings.database_name,
+                            password= settings.database_password,row_factory=dict_row  )
+         cursor = conn.cursor()
+         print("Connected to database successfully")
+         break
+     except Exception as error:
+         print("Failed to connect to database")
+         print("Error:", error)
+         time.sleep(2)
+
+
+
+
+
+
+
+
+
 
 SQLALCHEMY_DATABASE_URI = (f'postgresql://{settings.database_username}:{settings.database_password}'
                            f'@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
