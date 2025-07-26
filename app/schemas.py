14da8978d70571a008dc65ae25260dfcc43e43dd
Murage <davidmurage290@gmail.com>
2025-07-26 13:51:17 +0300

    adding fix to update quantity route v3

--- app/schemas.py
+++ app/schemas.py
@@ -53,7 +53,8 @@ class UpdateItemInventoryQuantity(BaseModel):
 
     model_config = {
         "populate_by_name": True,
-        "from_attributes": True
+        "from_attributes": True,
+        "allow_population_by_field_name": True
     }
 
 class UpdateItemInventoryPrice(BaseModel):
