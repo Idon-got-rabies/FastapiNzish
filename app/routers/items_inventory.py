6d5b6dcb89d8bbfc1b67defe20c5260af03b417c
Murage <davidmurage290@gmail.com>
2025-08-21 02:17:24 +0300

    adding search by name functionallity to search route v1.1.2.1

--- app/routers/items_inventory.py
+++ app/routers/items_inventory.py
@@ -102,12 +102,6 @@ async def search_inventory_by_id(query: str| int| None = Query(default=None, des
                      current_user = Depends(oauth2.get_current_user)):
 
     def sync_db():
-        if not current_user.is_admin:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Admin access required"
-            )
-
         if not query:
             raise HTTPException(status_code=404, detail="Item not found.")
 
